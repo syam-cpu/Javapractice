072207a22f01d5fe033d642070f36c9b6f96e678
syam-cpu <meghasyam2002@gmail.com>
2025-12-21 12:55:43 +0530

    project creation and println, print

---
+++ src/IntroTopics/HelloWorld.java
@@ -0,0 +1,12 @@
+package IntroTopics;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");// prints and moves the cursor to next line
+        System.out.println("Universe is good");
+
+        System.out.print("Hello World"); // prints but doesnt move the cursor
+        System.out.print("Universe is good");
+    }
+}
