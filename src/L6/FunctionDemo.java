ed0caeb70a8b763c00c149c303414398cd910a04
syam-cpu <meghasyam2002@gmail.com>
2026-01-04 18:09:53 +0530

    typecasting and function demo

---
+++ src/L6/FunctionDemo.java
@@ -0,0 +1,19 @@
+package L6;
+
+public class FunctionDemo {
+    public static void main(String[] args) {
+
+        System.out.println("Lets get started");
+        sayHello();// calling a function.
+        System.out.println("see you soon!");
+    }
+
+    //purpose : greating customers.
+    // process of declaring a function.
+    //20
+    public static void sayHello()
+    {
+        System.out.println("Hello to you!");
+        System.out.println("Hope you are having a good day!");
+    }
+}
---
+++ src/TypeCasting/typecasting.java
@@ -0,0 +1,67 @@
+package TypeCasting;
+
+public class typecasting {
+
+    public static void main(String[] args) {
+        byte by = 10;
+        short sh = 10;
+        int in = 10;
+        long lo = 10;
+
+       // bigger cannot go in to smaller:  by = sh; // cannot go directly needs human intervention.
+       // bigger cannot go in to smaller:  by = in;
+        // bigger cannot go in to smaller: by = lo;
+
+        sh = by; // smaller can in to bigger..automatically.
+        in = by;
+        lo = by;
+
+        float  f = 4.5f;
+        double d = 5.35;
+
+        // f = d // cannot write like this
+
+        d = f;
+
+        // cannot valid  directly in = f;
+
+        // forcefull conversion
+        in = (int)f;
+
+
+        byte b = (byte) 130;
+        System.out.println(b); // goes in a loop if any bigger number is assigned from out of range forcefully.
+
+        // case 5;
+
+        char ch  = 'a';
+        in = ch;
+        System.out.println(in);
+        System.out.println(ch);
+        System.out.println((int) ch);
+
+
+        ch = 70;
+        System.out.println(ch);
+        System.out.println((int)200); // typecasting is not needed.
+        ch = (char) (ch + 1);
+        System.out.println(ch);
+
+        ch = (char) (ch + 1);
+        System.out.println(ch);
+
+        ch = 'a' + 1;
+        System.out.println(ch);
+        ch = (char)(ch + 'a');
+        System.out.println(ch);
+
+        System.out.println(10 + 20 + "Hello" + 20 + 10);//30HEllo2010
+        System.out.println(2 + ' ' + 5);//39
+        System.out.println(2 + " " + 5);// 2 5
+        System.out.println("Hello" + '\t' + "World"); //  Hello World
+        System.out.println("Hello" + "\t" + "World"); // Hello World
+        System.out.println(2 + '\t' + 3); // 14
+
+
+    }
+}
